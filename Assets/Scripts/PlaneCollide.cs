7c74865aef9a9cca5e41e9c06616e91757b4d0fa
Eric <charmjunewonder@gmail.com>
2014-08-27 19:47:08 -0400

    wrong commit

--- Assets/Scripts/PlaneCollide.cs
+++ Assets/Scripts/PlaneCollide.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class PlaneCollide : MonoBehaviour {
-
+	Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -15,5 +15,8 @@ public class PlaneCollide : MonoBehaviour {
 
 	void OnTriggerEnter () {
 		Debug.Log ("oops");
+		//anim.SetBool ("isCrush", true);
+		//animation.Play ("PlaneCrush");
+		//Animator.
 	}
 }
