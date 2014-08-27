fba904269f18aba51755d8feace442b015116cf4
Eric <charmjunewonder@gmail.com>
2014-08-27 17:26:36 -0400

    StartGame scene first draft.

--- Assets/Scripts/StartSceneController.cs
+++ Assets/Scripts/StartSceneController.cs
@@ -2,19 +2,22 @@
 using System.Collections;
 
 public class StartSceneController : MonoBehaviour {
-
+	
 	void OnGUI () {
+		
 		// Make a background box
-		GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+		GUI.Box(new Rect(Screen.width/5, Screen.height/5, Screen.width*3/5,Screen.height*3/5), "Flight Game");
 		
 		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(20,40,80,20), "Level 1")) {
-			//Application.LoadLevel(1);
+		if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2-25,200,50), "Start")) {
+			Application.LoadLevel("Game");
 		}
 		
 		// Make the second button.
-		if(GUI.Button(new Rect(20,70,80,20), "Level 2")) {
-			//Application.LoadLevel(2);
+		if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2+50,200,50), "Exit")) {
+			print ("You clicked me!");
+			Application.Quit();
+			print ("You clicked me!");
 		}
 	}
 }
