cca7ed735734602050840fc3731b01675a5857e3
Eric <charmjunewonder@gmail.com>
2014-08-27 20:19:33 -0400

    Plane Crush Animation

--- Assets/Scripts/PlaneCollide.cs
+++ Assets/Scripts/PlaneCollide.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class PlaneCollide : MonoBehaviour {
 
+	Animator anim;
+
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -15,5 +17,6 @@ public class PlaneCollide : MonoBehaviour {
 
 	void OnTriggerEnter () {
 		Debug.Log ("oops");
+		anim.SetBool("isCrush", true);
 	}
 }
