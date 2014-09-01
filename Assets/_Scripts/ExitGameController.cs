813a593b5cb3ffc8b2d3fae5b2e4922055a3d231
Eric <charmjunewonder@gmail.com>
2014-09-01 15:07:24 -0400

    exit scene

--- Assets/_Scripts/ExitGameController.cs
+++ Assets/_Scripts/ExitGameController.cs
@@ -2,24 +2,25 @@
 using System.Collections;
 
 public class ExitGameController : MonoBehaviour {
+	public GUISkin mySkin;
 
 	void OnGUI () {
-		
-		// Make a background box
-		GUI.Box(new Rect(Screen.width/5, Screen.height/5, Screen.width*3/5,Screen.height*3/5), "Flight Game");
+		GUI.skin = mySkin;
 
-		GUI.Label(new Rect(Screen.width/2-100, Screen.height/2-100, 100, 20), "Score: " + Storage.score);
+		GUIStyle myStyle = new GUIStyle();
+		myStyle.normal.textColor = Color.white;
+		myStyle.fontSize = 100;
+		GUI.Label(new Rect(Screen.width/2-190, Screen.height/5-50, 100,20), "Flight Game", myStyle);
 
-		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-		if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2-25,200,50), "ReStart")) {
+		GUI.Label(new Rect(Screen.width/2-100, Screen.height/2-100, 200, 20), "Score: 1000" + Storage.score);
+
+		if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2-20,200,50), "Restart")) {
 			Application.LoadLevel("Game");
 		}
 		
-		// Make the second button.
-		if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2+50,200,50), "Exit")) {
+		if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2+100,200,50), "Exit")) {
 			print ("You clicked me!");
 			Application.Quit();
-			print ("You clicked me!");
 		}
 	}
 }
